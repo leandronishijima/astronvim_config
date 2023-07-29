27c1980d77dd066b279250bbe2a582fdc906f2b5
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-07-29 11:15:49 -0300

    add(community): noice + custom config

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,4 +8,32 @@ return {
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  {
+    "noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    cond = not vim.g.neovide,
+    opts = function(_, opts)
+      opts.messages = {
+        view_search = false,
+      }
+      opts.routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      }
+      opts.presets = {
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+        inc_rename = true,     -- enables an input dialog for inc-rename.nvim
+      }
+    end,
+  },
 }
