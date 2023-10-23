5fb0c2a3debfafb89d7a1fff8294549001aab9ac
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-10-23 19:34:07 -0300

    add(null-ls)(elixir): +buildtins.formatting.mimx

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -11,6 +11,7 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.mix,
       null_ls.builtins.diagnostics.rubocop,
     }
     return config -- return final config table
